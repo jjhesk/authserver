c5d1c17deff45a5e011453b7f2ff23652ad97403
Hesk <jobhesk@gmail.com>
2014-10-11 10:26:16 +0800

    remove the old unused files

--- js/adminmodel/GridContentEditor.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Hesk on 14年7月14日.
- */
-var GridContentEditor = GridContentEditor || {};
-jQuery(function ($) {
-    GridContentEditor = function ($container, templateController) {
-
-    };
-});
